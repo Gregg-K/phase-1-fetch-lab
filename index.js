0bef8f150690a348625d331c10c2b67acc98cae2
Gregg-k <gregory.k182@gmail.com>
2025-01-20 07:22:06 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  fetch('https://anapioficeandfire.com/api/books')
+    .then(response => response.json()) // 3. Convert the response to JSON
+    .then(data => {
+      // 4. Call the renderBooks function with the data
+      renderBooks(data);
+    })
+    .catch(error => {
+      console.error('Error fetching books:', error);
+    });
   
 }
 
